8bc68838a759e657c663be09cfca85bb4e271a74
Santiago Ariel Villanueva <santiagovillanuevaariel@gmail.com>
2023-08-04 10:59:26 -0300

    update darkmode

--- src/dark-mode.ts
+++ src/dark-mode.ts
@@ -1,13 +1,17 @@
 export const setupDarkModeToggle = (element: HTMLInputElement) => {
+  const matches = window.matchMedia("(prefers-color-scheme: dark)").matches;
   const darkModeOn =
-    localStorage.theme === "dark" ||
-    (!("theme" in localStorage) &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches);
+    localStorage.theme === "dark" || (!("theme" in localStorage) && matches);
 
   element.checked = darkModeOn;
-  element.addEventListener("change", () => {
+
+  const onChange = () => {
     document.documentElement.classList.toggle("dark", element.checked);
     localStorage.theme = element.checked ? "dark" : "light";
+  };
+
+  element.addEventListener("change", () => {
+    onChange();
   });
-  document.documentElement.classList.toggle("dark", element.checked);
+  onChange();
 };
